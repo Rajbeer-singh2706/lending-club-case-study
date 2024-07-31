7c17d6ce80606e9e96ec7e4ccfb009e70c448717
raj singh <rajsingh@rajs-MacBook-Air.local>
2024-07-31 14:51:08 +0530

    added evaluation

---
+++ evauation.sql
@@ -0,0 +1,6 @@
+#
+1. Data Understnading:                      10%
+2. Data Cleaning and Manipultion:           20%
+3. Data Analysis:                           40%
+4. Presentation and Recommendations:        20%
+5. Conciseness and readbility of the code:  10%
